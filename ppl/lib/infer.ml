94d414fb5e0ef168563c7004c271eb7ae175e59d
Anik Roy <anik545@gmail.com>
2019-12-18 21:27:11 +0000

    split inference procedures into separate modules

--- ppl/lib/infer.ml
+++ ppl/lib/infer.ml
@@ -1,7 +1,9 @@
+open Core
+
 module type Infer = sig 
   type 'a dist
   type prob = float
-  type 'a samples = 'a * prob 
+  type 'a samples = ('a * prob) list
   (* Inference is a procedure which transforms dists*)
   val infer: ?iterations:int -> 'a dist -> 'a dist
 end
@@ -14,6 +16,15 @@ end
 module CommonInference = struct 
   type 'a dist = 'a Dist.dist
   type prob = float
+  type 'a samples = ('a * prob) list
+
+  let (let*) = Dist.( let* )
+  let (>>=)  = Dist.(>>=)
+  (* TODO: clean this up - split dist into proper module + make this a functor *)
+  let fmap, return, categorical, sequence, bernoulli, mapM, condition, choice
+  = 
+  Dist.(fmap, return, categorical, sequence, bernoulli, mapM, condition, choice)
+  exception NotImplemented
 
   let rec prior': 'a.'a dist -> 'a dist = function
     Conditional (_,d) -> prior' d
@@ -32,7 +43,6 @@ let rec prior: 'a.'a dist -> ('a*prob) dist = function
       let* x = d in
       return (x,1.)
 
-type 'a samples = ('a * prob) list
 
 let resample (xs: 'a samples): ('a samples) dist =
   let n = List.length xs in
@@ -47,13 +57,16 @@ let flatten xss =
     | [] -> []
   in 
   flatten' xss
+
+let normalise xs = 
+  let norm = List.sum (module Float) ~f:snd xs in
+  List.map ~f:(fun (v,p)->(v,p/.norm)) xs
+  
 end
 
 module MH: Infer = struct
-  type 'a dist = 'a Dist.dist
-  type prob = float
-  type 'a samples = 'a * prob
   include CommonInference
+
   let mh n d =
     let proposal = prior d in
     let rec iterate ?(n=n) (x,s) =
@@ -66,6 +79,81 @@ module MH: Infer = struct
     in
     fmap (List.map ~f:fst) (proposal >>= iterate)
   
-  let infer ?(n=1000) d = fmap (fun x -> List.nth_exn x (n-1)) (mh n d)
+  let infer ?(iterations=1000) d = fmap (fun x -> List.nth_exn x (iterations-1)) (mh iterations d)
   
 end
+
+module SMC: Infer = struct
+  include CommonInference
+
+  let rec smc: 'a.int -> 'a dist -> 'a samples dist =
+    fun n ->
+    function
+    
+    | Conditional(c,d) ->
+      let updated = fmap normalise @@ 
+        condition (List.sum (module Float) ~f:snd) @@
+        let* ps = smc n d in
+        let qs = List.map ~f:(fun (x,w) -> (x, (c x) *. w)) ps in
+        return qs
+      in
+        updated >>= resample
+    
+    | Bind(d,f) ->
+        let* ps = smc n d in
+        let (xs, ws) = List.unzip ps in
+        let* ys = mapM f xs in
+        return (List.zip_exn ys ws)
+  
+    | d -> sequence @@ List.init n ~f:(fun _ -> (fmap (fun x-> (x, 1.)) d))
+  
+  let smc' n d = smc n d >>= categorical 
+  
+  (* let smcStandard n d = prior' (smc n d) *)
+  let infer ?(iterations=1000) d = prior' (smc' iterations d)
+end
+
+module PC: Infer = struct
+  include CommonInference
+
+  let resamplePC ps n = 
+    let rec iterate n mean ps iters =
+    match ps with
+    | [] -> raise NotImplemented
+    | (x,w)::ps ->
+      let k = float_of_int n in
+      let mean' = (k /. (k +. 1.)) *. mean +. (1. /. (k +. 1.)) *. w in
+      let r = w /. mean' in
+      let flr = Float.round_down r in
+      let probLow = flr in
+      let clr = Float.round_up r  in
+      let probHigh = clr in
+      let spawn x w =
+        if Float.(r < 1.) then
+          choice r (return [(x,mean')]) (return [])
+        else
+          choice (r -. probLow)
+          (return @@ List.init (int_of_float flr) ~f:(fun _ -> x, w /. probLow))
+          (return @@ List.init (int_of_float clr) ~f:(fun _ -> x, w /. probHigh))  
+      in
+      let* children = spawn x w in
+      if iters = 0 then return children else
+      let* rest = iterate (n+1) mean' ps (iters-1) in
+      return (children @ rest)
+    in
+      iterate 0 0. ps n
+  
+  let rec cascade:'a.int -> 'a dist -> 'a samples dist = fun n -> function
+    | Conditional(c,d) -> 
+        let* ps = cascade n d in
+        let qs = List.map ~f:(fun (x,w) -> (x,(c x) *. w )) ps in
+        resamplePC qs n
+    | Bind (d,f) ->
+        let* ps = cascade n d in
+        let (xs,ws) = List.unzip ps in
+        let* ys = mapM f xs in
+        return (List.zip_exn ys ws)
+  
+    | d -> sequence @@ List.init n ~f:(fun _ -> (fmap (fun x -> (x, 1.)) d))
+  
+  let infer ?(iterations=5000) d = (cascade iterations d) >>= (fun x -> categorical (List.take x iterations))end
