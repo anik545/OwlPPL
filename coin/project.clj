d60d633c316b15bfd99345bea8390ded23a99e25
Anik Roy <anik545@gmail.com>
2020-03-19 17:46:36 +0000

    .

---
+++ coin/project.clj
@@ -0,0 +1,11 @@
+(defproject coin "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "https://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"] [anglican "1.0.0"]
+]
+  :resource-paths ["/home/anik/anglican"]
+  :main ^:skip-aot coin.coin
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})pl
