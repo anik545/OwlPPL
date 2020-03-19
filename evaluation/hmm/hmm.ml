32786e1bb57cda2bc73ab4be284bd160a2dac14e
Anik Roy <anik545@gmail.com>
2020-03-19 17:46:36 +0000

    .

--- evaluation/hmm/hmm.ml
+++ evaluation/hmm/hmm.ml
@@ -6,20 +6,21 @@ let observe s = if s then bernoulli 0.9 else bernoulli 0.1
 
 type 'a hmm_model = {states:'a list; observations: 'a list}
 
-let rec hmm n = 
-  let* prev = match n with 
+let rec hmm n =
+  let* prev = match n with
       1 -> return ({states=[true];observations=[]})
     | _ -> hmm (n-1)
-  in 
+  in
 
-  let* new_state = trans (List.hd_exn prev.states)  in
+  let* new_state = trans (List.hd_exn prev.states) in
   let* new_obs = observe new_state in
   return ({states=(new_state::prev.states);observations=(new_obs::(prev.observations))})
 
-let model =  
+let model =
   let obs = [false;false;false] in
   let* r = hmm 3 in
   condition (Stdlib.(r.observations = obs))
     (return @@ List.rev r.states)
 
-let m = mh'' 100 model
+let m = mh'' 100000 model
+let x = sample m
