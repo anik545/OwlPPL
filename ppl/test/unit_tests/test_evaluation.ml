97c1d0733fa42c706f0f9ba330b0336a2f6a4141
Anik Roy <anik545@gmail.com>
2020-04-09 21:21:49 +0100

    remove linecount from git

--- ppl/test/unit_tests/test_evaluation.ml
+++ ppl/test/unit_tests/test_evaluation.ml
@@ -13,8 +13,8 @@ let test_chisq () = failwith "todo"
 
 let tests : unit Alcotest.test list =
   [
-    ("discrete kl-div", ["", `Quick, test_kl_continuous]);
-    ("continuous kl-div", ["", `Quick, test_kl_discrete]);
-    ("ks test statistic", ["", `Quick, test_ks]);
-    ("chi-sq test statistic", ["", `Quick, test_kl_discrete]);
+    ("discrete kl-div", [ ("", `Quick, test_kl_continuous) ]);
+    ("continuous kl-div", [ ("", `Quick, test_kl_discrete) ]);
+    ("ks test statistic", [ ("", `Quick, test_ks) ]);
+    ("chi-sq test statistic", [ ("", `Quick, test_kl_discrete) ]);
   ]
